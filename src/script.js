0a627f285fda6f68fcf403395e7135a072e88fd8
arnaud guillemin <arnaud.guillemin27@gmail.com>
2024-07-19 15:33:07 +0200

    modification couleur du background

--- src/script.js
+++ src/script.js
@@ -2,9 +2,9 @@ window.addEventListener('load', function() {
     const animatedLogo = document.getElementById('animatedLogo');
     const staticLogo = document.getElementById('staticLogo');
 
-    // Change 5000 to the duration you want (in milliseconds)
+    
     setTimeout(function() {
         animatedLogo.classList.add('hidden');
         staticLogo.classList.remove('hidden');
-    }, 3000); // 5 seconds
+    }, 3000); 
 });
