3267d84cc785c7e3e5aa4a5efb761872a72f1963
Antoine Jouan <antoine.jouan@neatcraft.fr>
2026-03-25 12:01:29 +0100

    feat: implem nouveau header

--- panda.config.ts
+++ panda.config.ts
@@ -11,74 +11,12 @@ export default defineConfig({
     exclude: [],
 
     patterns: {
-        extend: {
-            section: {
-                description: "section container",
-                defaultValues: {
-                    marginTop: "0",
-                    bgColor: "primary.100",
-                    flexDir: "row",
-                },
-                properties: {
-                    marginTop: {type: "string"},
-                    bgColor: {type: "string"},
-                    flexDir: {type: "enum", value: ["column", "row"]}
-                },
-                transform(prop) {
-                    return {
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "center",
-                        padding: "3% 2% 7% 2%",
-                        "&:target": {
-                            _portrait: {
-                                scrollMarginTop: "15vh",
-                            },
-                            _landscape: {
-                                scrollMarginTop: {xs: "15vw", lg: "5vw"},
-                            },
-                        },
-                        ...prop
-                    }
-                }
-            },
-            title: {
-                description: "title",
-                defaultValues: {
-                    color: "white"
-                },
-                properties: {
-                    color: {type: "string"},
-                },
-                transform(props) {
-                    return {
-                        display: "block",
-                        fontFamily: "barlow",
-                        fontSize: {xs: "3rem", lg: "7rem"},
-                        lineHeight: "1",
-                        fontWeight: "500",
-                        textTransform: "uppercase",
-                        textAlign: {xs:"center",sm:"center",md:"center"},
-                            ...props
-                    }
-                }
-            },
-            paragraph: {
-                description: "paragraph",
-                transform(props) {
-                    return {
-                        fontFamily: "montserrat",
-                        fontSize: {xs: "1.2rem", lg: "2rem"},
-                        textAlign: 'center',
-                        fontWeight: "400",
-                        lineHeight: {xs: "1.5rem", md: "2.7rem"},
-                        marginTop: "1rem",
-                        marginBottom: "2rem",
-                        ...props
-                    }
-                }
-            },
-        }
+
+    },
+
+    globalVars: {
+        '--font-montserrat': 'montserrat',
+        '--font-barlow': 'barlow',
     },
 
     // Useful for theme customization
@@ -95,144 +33,117 @@ export default defineConfig({
             tokens: {
                 fonts: {
                     montserrat: {
-                        value: 'var(--font-montserrat)'
+                        value: "var(--font-montserrat), sans-serif",
+                    },
+                    barlow: {
+                        value: "var(--font-barlow), sans-serif",
                     }
                 },
                 colors: {
                     primary: {
-                        100: {value: "#2a3159"},
-                        200: {value: "#3a3f6b"},
-                        300: {value: "#4a4f7f"},
-                        400: {value: "#5a5f8f"},
-                        500: {value: "#6a6f9f"},
-                        600: {value: "#7a7fa0"},
-                        700: {value: "#8a8fa1"},
-                        800: {value: "#9a9fa2"},
-                        900: {value: "#aaaaa3"},
-                        950: {value: "#bbbbb4"},
+                        5: {value: "rgb(0 39 64 / 0.05)"},
+                        70: { value: "rgb(0 39 64 / 0.7)" },
+                        100: { value: "rgb(0 39 64)" }
                     },
-                    secondary: {
-                        100: {value: "#ed7d1a"},
-                        200: {value: "#ef8a31"},
-                        300: {value: "#f19748"},
-                        400: {value: "#f2a45f"},
-                        500: {value: "#f4b176"},
-                        600: {value: "#f6be8c"},
-                        700: {value: "#f8cba3"},
-                        800: {value: "#fad8ba"},
-                        900: {value: "#fbe5d1"},
-                        950: {value: "#fdf2e8"}
-                    },
-                    third: {
-                        100: {value: "#7d41aa"},
-                        200: {value: "#8b4cbb"},
-                        300: {value: "#9860c2"},
-                        400: {value: "#a574ca"},
-                        500: {value: "#b288d1"},
-                        600: {value: "#bf9bd9"},
-                        700: {value: "#ccafe1"},
-                        800: {value: "#d8c3e8"},
-                        900: {value: "#e5d7f0"},
-                        950: {value: "#f2ebf7"}
-                    }
+                    secondary: { value: "#ed7d1a" },
+                    third: { value: "#5d358f"},
+                    fourth: { value: "#ef0098" },
+                    fifth: { value: "#460098" },
                 },
+                radii : { value: "0.75rem" }
             },
         },
-    },
-
-    globalVars: {
-        '--font-montserrat': 'montserrat'
     },
 
     globalFontface: {
         barlow: [
             {
-                src: "url(/fonts/barlow/BarlowCondensed-Thin.ttf) format('truetype')",
+                src: "url(/fonts/barlow/Barlow-Thin.ttf) format('truetype')",
                 fontWeight: "100",
                 fontStyle: "normal",
                 fontDisplay: "swap"
             }, {
-                src: "url(/fonts/barlow/BarlowCondensed-ThinItalic.ttf) format('truetype')",
+                src: "url(/fonts/barlow/Barlow-ThinItalic.ttf) format('truetype')",
                 fontWeight: "100",
                 fontStyle: "italic",
                 fontDisplay: "swap"
             }, {
-                src: "url(/fonts/barlow/BarlowCondensed-ExtraLight.ttf) format('truetype')",
+                src: "url(/fonts/barlow/Barlow-ExtraLight.ttf) format('truetype')",
                 fontWeight: "200",
                 fontStyle: "normal",
                 fontDisplay: "swap"
             }, {
-                src: "url(/fonts/barlow/BarlowCondensed-ExtraLightItalic.ttf) format('truetype')",
+                src: "url(/fonts/barlow/Barlow-ExtraLightItalic.ttf) format('truetype')",
                 fontWeight: "200",
                 fontStyle: "italic",
                 fontDisplay: "swap"
             }, {
-                src: "url(/fonts/barlow/BarlowCondensed-Light.ttf) format('truetype')",
+                src: "url(/fonts/barlow/Barlow-Light.ttf) format('truetype')",
                 fontWeight: "300",
                 fontStyle: "normal",
                 fontDisplay: "swap"
             }, {
-                src: "url(/fonts/barlow/BarlowCondensed-LightItalic.ttf) format('truetype')",
+                src: "url(/fonts/barlow/Barlow-LightItalic.ttf) format('truetype')",
                 fontWeight: "300",
                 fontStyle: "italic",
                 fontDisplay: "swap"
             }, {
-                src: "url(/fonts/barlow/BarlowCondensed-Regular.ttf) format('truetype')",
+                src: "url(/fonts/barlow/Barlow-Regular.ttf) format('truetype')",
                 fontWeight: "400",
                 fontStyle: "normal",
                 fontDisplay: "swap"
             }, {
-                src: "url(/fonts/barlow/BarlowCondensed-Italic.ttf) format('truetype')",
+                src: "url(/fonts/barlow/Barlow-Italic.ttf) format('truetype')",
                 fontWeight: "400",
                 fontStyle: "italic",
                 fontDisplay: "swap"
             }, {
-                src: "url(/fonts/barlow/BarlowCondensed-Medium.ttf) format('truetype')",
+                src: "url(/fonts/barlow/Barlow-Medium.ttf) format('truetype')",
                 fontWeight: "500",
                 fontStyle: "normal",
                 fontDisplay: "swap"
             }, {
-                src: "url(/fonts/barlow/BarlowCondensed-MediumItalic.ttf) format('truetype')",
+                src: "url(/fonts/barlow/Barlow-MediumItalic.ttf) format('truetype')",
                 fontWeight: "500",
                 fontStyle: "italic",
                 fontDisplay: "swap"
             }, {
-                src: "url(/fonts/barlow/BarlowCondensed-SemiBold.ttf) format('truetype')",
+                src: "url(/fonts/barlow/Barlow-SemiBold.ttf) format('truetype')",
                 fontWeight: "600",
                 fontStyle: "normal",
                 fontDisplay: "swap"
             }, {
-                src: "url(/fonts/barlow/BarlowCondensed-SemiBoldItalic.ttf) format('truetype')",
+                src: "url(/fonts/barlow/Barlow-SemiBoldItalic.ttf) format('truetype')",
                 fontWeight: "600",
                 fontStyle: "italic",
                 fontDisplay: "swap"
             }, {
-                src: "url(/fonts/barlow/BarlowCondensed-Bold.ttf) format('truetype')",
+                src: "url(/fonts/barlow/Barlow-Bold.ttf) format('truetype')",
                 fontWeight: "700",
                 fontStyle: "normal",
                 fontDisplay: "swap"
             }, {
-                src: "url(/fonts/barlow/BarlowCondensed-BoldItalic.ttf) format('truetype')",
+                src: "url(/fonts/barlow/Barlow-BoldItalic.ttf) format('truetype')",
                 fontWeight: "700",
                 fontStyle: "italic",
                 fontDisplay: "swap"
             }, {
-                src: "url(/fonts/barlow/BarlowCondensed-ExtraBold.ttf) format('truetype')",
+                src: "url(/fonts/barlow/Barlow-ExtraBold.ttf) format('truetype')",
                 fontWeight: "800",
                 fontStyle: "normal",
                 fontDisplay: "swap"
             }, {
-                src: "url(/fonts/barlow/BarlowCondensed-ExtraBoldItalic.ttf) format('truetype')",
+                src: "url(/fonts/barlow/Barlow-ExtraBoldItalic.ttf) format('truetype')",
                 fontWeight: "800",
                 fontStyle: "italic",
                 fontDisplay: "swap"
             }, {
-                src: "url(/fonts/barlow/BarlowCondensed-Black.ttf) format('truetype')",
+                src: "url(/fonts/barlow/Barlow-Black.ttf) format('truetype')",
                 fontWeight: "900",
                 fontStyle: "normal",
                 fontDisplay: "swap"
             }, {
-                src: "url(/fonts/barlow/BarlowCondensed-BlackItalic.ttf) format('truetype')",
+                src: "url(/fonts/barlow/Barlow-BlackItalic.ttf) format('truetype')",
                 fontWeight: "900",
                 fontStyle: "italic",
                 fontDisplay: "swap"
