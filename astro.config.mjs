0ec1fe1d38fddb89df587ba580ad8ed1d15871b0
Antoine Jouan <antoine.jouan@neatcraft.fr>
2026-05-13 15:06:25 +0200

    feat: page transfo si

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,4 @@
 import { defineConfig } from 'astro/config';
-import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config alt="liens vers astro"
-export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
-    },
-});
+export default defineConfig();
